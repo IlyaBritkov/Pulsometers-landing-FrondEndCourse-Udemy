62f363b560556279141c2a37d70f6d3b717beb13
Ilya <ilya500514@gmail.com>
2020-12-26 01:55:56 +0300

    Slick-slider changed to Tini-slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,17 @@
-
-$(document).ready(function(){
-    $('.carousel__inner').slick({
-        infinite: true,
-        speed: 800,
-        slidesToShow: 1,
-        // adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="img/watchs_carousel/arrow_left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="img/watchs_carousel/arrow_right.png"></button>',
-        responsive: [
-            {
-              breakpoint: 992,
-              settings: {
-                dots: true,
-                arrows:false
-              }
-            }
-          ]
-    });
+// Tini-slider
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false
   });
+
+  document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+  });
+
+  document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
+  })
